93afa0c2bb8e5f03052976a9fbd944af2dee187b
NoahNovick <nnovickfaille1@gmail.com>
2020-01-16 21:18:13 -0500

    function for odd numbers

---
+++ odd.js
@@ -0,0 +1,8 @@
+let isOdd = function (num){
+  return num % 2 === 1;
+}
+let tenIsEven = isOdd(10);
+let elevenIsOdd = isOdd(11);
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
