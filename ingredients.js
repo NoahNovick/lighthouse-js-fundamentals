019a0ed98d46a5dab5431269fdc845657336f41c
NoahNovick <nnovickfaille1@gmail.com>
2020-01-20 12:27:08 -0500

    practice for loops over arrays

--- ingredients.js
+++ ingredients.js
@@ -1,6 +1,4 @@
 const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
-let i =0;
-while(i < ingredients.length){
-  console.log(ingredients[1]);
-  i++
+for(let i=0; i < ingredients.length; i++ ){
+  console.log(ingredients[i])
 }
